820ad1532ccaa71f2dd495554066d23d1ffc5a83
ANIT RAJ <88747708+AnitRaj-001@users.noreply.github.com>
2026-03-13 18:20:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/AnitRaj-001.github.io/'
-    : '/',
+  base: '/AnitRaj-001.github.io/',
   plugins: [react()]
 })
